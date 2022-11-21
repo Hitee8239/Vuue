14b6bf046aaa20c2d6138f67af68c82f1640d63e
hitee <laby8239@gmail.com>
2022-11-21 23:35:37 +0900

    유효성검사

--- src/components/auth/SignUpForm.vue
+++ src/components/auth/SignUpForm.vue
@@ -1,33 +1,49 @@
 <template>
-    <v-form @submit.prevent="save" ref="form" v-model="valid" lazy-validation>
-      <v-btn type="submit" block color="primary">회원가입</v-btn>
-    </v-form>
-  </template>
-  
-  <script>
-  export default {
-    name: "SignUpForm",
-    data() {
-      return {
-        valid: true,
-        form: {
-          mb_id: "",
-          mb_password: "",
-          mb_name: "",
-          mb_birth: "",
-          mb_gender: "",
-          mb_email: "",
-          mb_phone: "",
-          mb_zip: "",
-          mb_addr1: "",
-          mb_addr2: "",
-        },
-      };
-    },
-    methods: {
-      save() {
-        console.log(this.form);
+  <v-form @submit.prevent="save" ref="form" v-model="valid" lazy-validation>
+    <input-duplicate-check
+      v-model="form.mb_id"
+      label="아이디"
+      prepend-icon="mdi-account"
+      counter="30"
+      :rules="rules.id()"
+    />
+    <v-btn type="submit" block color="primary">회원가입</v-btn>
+  </v-form>
+</template>
+
+<script>
+import validateRules from '../../../util/validateRules'
+import InputDuplicateCheck from "../InputForms/InputDuplicateCheck.vue";
+export default {
+  components: { InputDuplicateCheck },
+  name: "SignUpForm",
+  data() {
+    return {
+      valid: true,
+      form: {
+        mb_id: "",
+        mb_password: "",
+        mb_name: "",
+        mb_birth: "",
+        mb_gender: "",
+        mb_email: "",
+        mb_phone: "",
+        mb_zip: "",
+        mb_addr1: "",
+        mb_addr2: "",
       },
+    };
+  },
+  computed : {
+    rules : ()=> validateRules,
+  },
+  methods: {
+    async save() {
+      this.$refs.form.validate();
+      await this.$nextTick();
+      if(!this.valid) return;
+      console.log(this.form)
     },
-  };
-  </script>
+  },
+};
+</script>
